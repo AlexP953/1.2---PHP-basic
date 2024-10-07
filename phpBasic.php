253653b007c857f00c75858a521874312e64b234
alexperis95 <alexperis95@gmail.com>
2024-10-07 15:56:54 +0200

    Nivell 2

--- phpBasic.php
+++ phpBasic.php
@@ -8,15 +8,16 @@ $palabra = 'Estring';
 $boleano = true;
 
 define('myName','Alex Peris');
-echo '<h1>Exercici 1</h1>';
+echo '<h1 style="font-size: 3rem; color: red;">Nivell 1</h1>';
+echo '<h2>Exercici 1</h2>';
 echo 'integer -> '.$entero.'<br>';
 echo 'double -> '.$doble.'<br>';
 echo 'string -> '.$palabra.'<br>';
 echo 'boolean -> '.$boleano.'<br>';
-echo '<h2>'.myName.'</h2>';
+echo '<h3>'.myName.'</h3>';
 
 // Exercici 2
-echo '<h1>Exercici 2</h1>';
+echo '<h2>Exercici 2</h2>';
 
 $helloWorld = "Hello, World!";
 $separacion = '<br>';
@@ -34,7 +35,7 @@ $secondVar = 'Aquest és el curs de PHP';
 echo $helloWorld.' '.$secondVar; // Crea una nova variable amb el contingut “Aquest és el curs de PHP” i imprimeix per pantalla la concatenació de tots dos strings
 
 // Exercici 3
-echo '<h1>Exercici 3</h1>';
+echo '<h2>Exercici 3</h2>';
 echo $separacion;
 
 
@@ -92,7 +93,7 @@ function calculator($param1,$param2,$method){
 echo calculator(5,10,'/');
 
 // Exercici 4
-echo '<h1>Exercici 4</h1>';
+echo '<h2>Exercici 4</h2>';
 echo $separacion;
 
 // Fes un programa que implementi una funció on es compti fins a un nombre determinat. Si no s’inclou un nombre determinat, el nombre haurà de tenir un valor per defecte igual a 10. A més, aquesta funció ha de tenir un segon paràmetre que indiqui de quant a quant es compta(D'1 en 1, de 2 en 2…). El compte s’ha de mostrar per pantalla pas per pas.
@@ -107,7 +108,7 @@ counter(15, 1);
 
 
 // Exercici 5
-echo '<h1>Exercici 5</h1>';
+echo '<h2>Exercici 5</h2>';
 echo $separacion;
 
 // Escriure una funció per verificar el grau d'un/a estudiant d'acord amb la nota.
@@ -132,7 +133,7 @@ function StudentGrade($note){
 echo StudentGrade(45);
 
 // Exercici 6
-echo '<h1>Exercici 6</h1>';
+echo '<h2>Exercici 6</h2>';
 echo $separacion;
 
 // Escriu La funció isBitten () que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari.
@@ -148,4 +149,70 @@ function isBitten(){
 }
 
 echo isBitten();
+
+echo '<h1 style="font-size: 3rem; color: red;">Nivell 2</h1>';
+
+// Exercici 1
+echo '<h2>Exercici 1</h2>';
+echo $separacion;
+
+// Escriu una funció que determini la quantitat total a pagar per una trucada telefònica segons les següents premisses:
+
+// Tota trucada que duri menys de 3 minuts té un cost de 10 cèntims.
+// Cada minut addicional a partir dels 3 primers és un pas de comptador i costa 5 cèntims.
+
+function calculateCallPhone($minutes){
+  $totalCost = 0.10;
+  $secondStretch;
+
+  if ($minutes > 3){
+    $secondStretch = $minutes - 3;
+    $totalCost = $totalCost + $secondStretch * 0.05;
+  }
+  return $totalCost;
+}
+
+echo 'El cost de la trucada es: '.calculateCallPhone(4).'€';
+
+// Exercici 2
+echo '<h2>Exercici 2</h2>';
+echo $separacion;
+
+
+// Imagina que som a una botiga on es ven:
+
+// Xocolata: 1 euro
+// Xiclets: 0,50 euros
+// Caramels: 1,50 euros
+// Implementa un programa que permeti afegir càlculs a un total de compres d'aquest tipus. Per exemple, que si comprem:
+
+// 2 xocolates, 1 de xiclets i 1 de caramels, tinguem un programa que permeti anar afegint els subtotals a un total, tal que així:
+
+// funció(2 xocolates) + funció(1 de xiclets) + funció(1 de carmels) = 2 + 0.5 + 1.5
+
+// Sent, per tant, el total, 4.
+
+$totalAddToCart = 0;
+
+function addToCart ($amount, $product) {
+  global $totalAddToCart;
+  switch ($product) {
+    case 'Xocolata':
+      $totalAddToCart += $amount * 1;
+      break;
+    case 'Xiclets':
+      $totalAddToCart += $amount * 0.5;
+      break;
+    case 'Caramels':
+      $totalAddToCart += $amount * 1.5;
+      break;
+  }
+}
+
+echo $separacion;
+addToCart(2, 'Xocolata');
+addToCart(1,'Xiclets');
+addToCart(1,'Caramels');
+echo $totalAddToCart.'€ perque ara hi han 2 xocolatas, 1 xiclet i 1 carmel.' 
+
 ?>
